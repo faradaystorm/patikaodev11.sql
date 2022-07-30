2b90356a9e793f705e1bd9a5c5de6351e16744c8
Esma Şen <96180440+faradaystorm@users.noreply.github.com>
2022-07-30 14:47:14 +0300

    Create odev1.3.sql

---
+++ odev1.3.sql
@@ -0,0 +1,4 @@
+--actor ve customer tablolarında bulunan first_name sütunları için ilk tabloda bulunan ancak ikinci tabloda bulunmayan verileri sıralayalım.
+SELECT first_name FROM actor
+INTERSECT
+SELECT first_name FROM customer;
