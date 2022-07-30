d61fcad99f1696848cfac3accda5402064ad86f5
Esma Şen <96180440+faradaystorm@users.noreply.github.com>
2022-07-30 14:48:13 +0300

    Create odev1.4.sql

---
+++ odev1.4.sql
@@ -0,0 +1,12 @@
+--İlk 3 sorguyu tekrar eden veriler için de yapalım.
+SELECT first_name FROM actor
+UNION ALL
+SELECT first_name FROM customer;
+
+SELECT first_name FROM actor
+INTERSECT ALL
+SELECT first_name FROM customer;
+
+SELECT first_name FROM actor
+EXCEPT ALL
+SELECT first_name FROM customer;
